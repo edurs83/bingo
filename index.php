8fb5526ba6f2ac325a9a2569133ee2ccaba71b33
Eduardo <edurs83@gmail.com>
2014-11-12 18:17:02 +0100

    Añadido css y las sesiones para generar números

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
+<?php
+	session_name("Bingo");
+	session_start();
+
+	if(isset($_POST["Nuevo_juego"])){
+		session_destroy();
+		session_start();
+	}
+
+	if (!isset($_SESSION['numeros'])) {
+		$_SESSION['numeros'] = array();
+	}
+
+	if (!isset($_SESSION['x'])) {
+		$_SESSION['x'] = 0;
+	}
+	if (!isset($_SESSION['texto'])) {
+		$_SESSION['texto'] = '<span id="inicio"/>';
+	}
+	
+	if (isset($_POST['obtener_numero'])) {
+		do {
+           	$_SESSION['x'] = rand(1, 90);
+        } while (in_array($_SESSION['x'], $_SESSION['numeros'])); 
+		array_push($_SESSION['numeros'], $_SESSION['x']);
+		$_SESSION['texto'] = '<span id="numero_actual">'.$_SESSION['x'].'</span>';
+	}
+?>
+
+
 <!DOCTYPE>
 <html lang="es">
 <head>
 	<title>Eduardo-Bingo</title>
+	<link rel="stylesheet" type="text/css" href="bingo.css"/>
 	<meta charset="utf-8">
 </head>
 <body>
-
+	<form action="index.php" method="post">
 	<?php
+		echo '<label>'.$_SESSION['texto'].'</label>';
 		echo '<table border="1">';
 			for ($i=0; $i<11; $i++) 
 			{
@@ -31,5 +63,8 @@
 		echo '</table>';
 	?>
 
+	<button type="submit" name="obtener_numero">Obtener Numero</button>
+	<button type="submit" name="Nuevo_juego">Nuevo Juego</button>
+	</form>
 </body>
 </html>
